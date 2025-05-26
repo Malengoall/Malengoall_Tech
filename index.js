339601e2059f13f89d6e264914826c444667050f
Malengoall <malengoally87@gmail.com>
2025-05-26 10:47:11 +0300

    Update index.js
    const { AntiDelDB,
        initializeAntiDeleteSettings,
        setAnti,
        getAnti,
        getAllAntiDeleteSettings, } = require('./antidel');
    const {
        saveContact,
        loadMessage,
        getName,
        getChatSummary,
        saveGroupMetadata,
        getGroupMetadata,
        saveMessageCount,
        getInactiveGroupMembers,
        getGroupMembersMessageCount,
        saveMessage,
    } = require('./store');
    
    
    module.exports = {
        AntiDelDB,
        initializeAntiDeleteSettings,
        setAnti,
        getAnti,
        getAllAntiDeleteSettings,
        saveContact,
        loadMessage,
        getName,
        getChatSummary,
        saveGroupMetadata,
        getGroupMetadata,
        saveMessageCount,
        getInactiveGroupMembers,
        getGroupMembersMessageCount,
        saveMessage,
    };

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-const { default: makeWASocket, useSingleFileAuthState, DisconnectReason } = require('@whiskeysockets/baileys'const { default: makeWASocket, useSingleFileAuthState, DisconnectReason } = require('@whiskeysockets/baileys')
-const { Boom } = require('@hapi/boom')
-const { join } = require('path')
+const { AntiDelDB,
+    initializeAntiDeleteSettings,
+    setAnti,
+    getAnti,
+    getAllAntiDeleteSettings, } = require('./antidel');
+const {
+    saveContact,
+    loadMessage,
+    getName,
+    getChatSummary,
+    saveGroupMetadata,
+    getGroupMetadata,
+    saveMessageCount,
+    getInactiveGroupMembers,
+    getGroupMembersMessageCount,
+    saveMessage,
+} = require('./store');
 
-const { state, saveState } = useSingleFileAuthState('./auth.json')
 
-async function startSock() {
-    const sock = makeWASocket({
-        auth: state,
-        printQRInTerminal: true
-    })
-
-    sock.ev.on('creds.update', saveState)
-
-    sock.ev.on('connection.update', (update) => {
-        const { connection, lastDisconnect } = update
-        if(connection === 'close') {
-            const shouldReconnect = (lastDisconnect.error = Boom)?.output?.statusCode !== DisconnectReason.loggedOut
-            console.log('connection closed due to ', lastDisconnect.error, ', reconnecting', shouldReconnect)
-            if(shouldReconnect) startSock()
-        } else if(connection === 'open') {
-            console.log('✅ Bot connected!')
-        }
-    })
-}
-
-startSock()
+module.exports = {
+    AntiDelDB,
+    initializeAntiDeleteSettings,
+    setAnti,
+    getAnti,
+    getAllAntiDeleteSettings,
+    saveContact,
+    loadMessage,
+    getName,
+    getChatSummary,
+    saveGroupMetadata,
+    getGroupMetadata,
+    saveMessageCount,
+    getInactiveGroupMembers,
+    getGroupMembersMessageCount,
+    saveMessage,
+};￼Enter
