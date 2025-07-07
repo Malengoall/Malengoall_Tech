5752839758be5323eea45a397036ce1dd1b9c1c3
Malengoall <malengoally87@gmail.com>
2025-07-07 09:26:35 +0300

    Update color.js

--- Start/lib/color.js
+++ Start/lib/color.js
@@ -1,6 +1,19 @@
-const chalk = require('chalk');
+const chalk = require('chalk')
+
+const color = (text, color) => {
+    return !color ? chalk.blue(text) : chalk.keyword(color)(text)
+}
+
+const bgcolor = (text, bgcolor) => {
+  return !bgcolor ? chalk.blue(text) : chalk.bgKeyword(bgcolor)(text)
+}
+
+const Lognyong = (text, color) => {
+  return !color ? chalk.red('[ ! ] ') + chalk.blue(text) : chalk.red('=> ') + chalk.keyword(color)(text)
+}
 
 module.exports = {
-  red: (text) => chalk.red(text),
-  blue: (text) => chalk.blue(text),
-};
+  color,
+  bgcolor,
+  Lognyong
+}
