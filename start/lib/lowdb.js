33be1768830d84aa1e4e8628718c4862b789327b
Malengoall <malengoally87@gmail.com>
2025-07-07 12:16:45 +0300

    Update lowdb.js

--- start/lib/lowdb.js
+++ start/lib/lowdb.js
@@ -1,7 +1,26 @@
-const db = require('./start/lib/lowdb');
+const { Low, JSONFile } = require('lowdb');
+const path = require('path');
+const fs = require('fs');
 
-async function saveChat(chat) {
+// Define path ya database file
+const file = path.join(__dirname, '../../db.json');
+
+// Hakikisha file la DB lipo, kama halipo liumbe
+if (!fs.existsSync(file)) {
+  fs.writeFileSync(file, JSON.stringify({ chats: [] }, null, 2));
+}
+
+// Setup adapter na instance ya LowDB
+const adapter = new JSONFile(file);
+const db = new Low(adapter);
+
+// Initialize database
+async function initDB() {
   await db.read();
-  db.data.chats.push(chat);
+  db.data ||= { chats: [] };
   await db.write();
 }
+initDB();
+
+// Export database ili utumie sehemu nyingine
+module.exports = db;
